8f182e8ebb38837a48399a13093a6d60ef8ceaee
Raph <raphuzanjr@gmail.com>
2019-12-31 15:10:11 -0800

    isolated the backend to different repo

--- src/Nav.js
+++ src/Nav.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Nav.css';
+import logo from './index.png';
 class Nav extends React.Component {
 
     render() {
 
         return(
             <div id="nav">
+                <Link to="/"><img id="Logo" src={logo} alt="Logo"/></Link>
                 <Link to="/signup">SignUp</Link>
-                <Link to="/">Home</Link>
                 <Link to="/login">Login</Link>
             </div>
     );}
